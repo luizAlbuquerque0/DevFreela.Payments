3def4873bb548e0bc3262dc4db70d9223eebe19a
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2022-08-22 15:03:20 -0300

    Feature Implement MessageBus

---
+++ DevFreela.Payments.API/Models/PaymentAprovedIntegrationEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace DevFreela.Payments.API.Models
+{
+    public class PaymentAprovedIntegrationEvent
+    {
+        public PaymentAprovedIntegrationEvent(int idProject)
+        {
+            IdProject = idProject;
+        }
+
+        public int IdProject { get; set; }
+    }
+}
